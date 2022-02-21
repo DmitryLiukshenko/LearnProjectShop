25cb2a74af106cbbd5a88521156f3dab7f46608d
CreatorDL <dmitriiliukshenko@gmail.com>
2022-02-21 19:50:07 +0700

    Тест

---
+++ BeautyShop/AppDataFile/ConnectOdb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyShop.AppDataFile
+{
+    class ConnectOdb 
+    {
+        public static BeautyShopEntities conObj;
+    }
+}
---
+++ BeautyShop/Pages/PageEditNew.xaml.cs
@@ -0,0 +1,98 @@
+﻿using BeautyShop.AppDataFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeautyShop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageEditNew.xaml
+    /// </summary>
+    public partial class PageEditNew : Page
+    {
+        bool LogicRB = false;
+        public PageEditNew(Product product)
+        {
+            InitializeComponent();
+
+            CmbxOwner.SelectedIndex = (int)product.ManufacturerID - 1;
+            CmbxOwner.SelectedValuePath = "ID";
+            CmbxOwner.DisplayMemberPath = "Name";
+            CmbxOwner.ItemsSource = ConnectOdb.conObj.Manufacturer.ToList();
+
+            ProductObj.Id = product.ID;
+
+            TxtTitle.Text = product.Title;
+            TxtCost.Text = Convert.ToString(product.Cost);
+            TxtImage.Text = product.MainImagePath;
+            TxtDescription.Text = product.Description;
+
+            if (product.IsActive != false)
+            {
+                RbActive.IsChecked = true;
+                LogicRB = true;
+            }
+            else {
+                RbActive.IsChecked = true;
+                LogicRB=false;
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IEnumerable<Product> products = ConnectOdb.conObj.Product.Where(x => x.ID == ProductObj.Id).AsEnumerable().Select(x =>
+                {
+                    x.Title = TxtTitle.Text;
+                    x.Cost = Convert.ToDecimal(TxtCost.Text);
+
+                    if (string.IsNullOrWhiteSpace(TxtDescription.Text))
+                    {
+                        x.Description = "";
+                    }
+                    else
+                    {
+                        x.Description = TxtDescription.Text;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(TxtImage.Text))
+                    {
+                        x.MainImagePath = "";
+                    }
+                    else
+                    {
+                        x.MainImagePath = TxtImage.Text;
+                    }
+
+                    x.IsActive = LogicRB;
+                    x.ManufacturerID = (int)CmbxOwner.SelectedValue;
+                    return x;
+                });
+
+                foreach (Product prdct in products)
+                {
+                    ConnectOdb.conObj.Entry(prdct).State = System.Data.Entity.EntityState.Modified;
+                }
+                ConnectOdb.conObj.SaveChanges();
+                MessageBox.Show("Данные успешно изменены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message.ToString());
+            }
+        }
+                
+    }
+}
